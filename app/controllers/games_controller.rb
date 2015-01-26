0baa487532eb3c7e8557488a9dab916cc9271eac
Gebegebe <kosminska.p@gmail.com>
2015-01-26 20:14:15 +0100

    [#3] Added index, show and modified create in GamesController. Created index and show views of GameController.
    Signed-off-by: Gebegebe <kosminska.p@gmail.com>

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,24 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:game].inspect
+    @game = Game.new(game_params)
+ 
+    @game.save
+    redirect_to @game
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:title, :description)
   end
 end
