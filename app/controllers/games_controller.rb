11d4ec1eaf02198125f195c773c04f8b41a2e4f6
Gebegebe <kosminska.p@gmail.com>
2015-01-26 20:56:38 +0100

    [#5] Added partial view, update and destroy to Game. Added validation to Game.
    Signed-off-by: Gebegebe <kosminska.p@gmail.com>

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,13 +8,38 @@ class GamesController < ApplicationController
   end
 
   def new
+    @game = Game.new
+  end
+
+  def edit
+    @game = Game.find(params[:id])
   end
 
   def create
     @game = Game.new(game_params)
  
-    @game.save
-    redirect_to @game
+    if @game.save
+      redirect_to @game
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @game = Game.find(params[:id])
+   
+    if @game.update(game_params)
+      redirect_to @game
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+   
+    redirect_to games_path
   end
 
   private
