4d9a7ee023e851e5d9919bd4baca025c74f5f947
Gebegebe <kosminska.p@gmail.com>
2015-01-27 19:03:44 +0100

    [#7] Added model for User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127165059) do
+ActiveRecord::Schema.define(version: 20150127175422) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20150127165059) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "pass"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
