7b70c2d2fed68e9c44c6560bfd2cd8023d01f7a9
Gebegebe <kosminska.p@gmail.com>
2015-01-31 16:23:58 +0100

    [#8] Added bootstrap gem. Started signup form for users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX },
-    uniqueness: { case_sensitive: false },
-  validates :pass, length: { minimum: 6 }
-  has_secure_password
+    uniqueness: { case_sensitive: false }
+  validates :pass, presence:true, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'signup'  => 'users#new'
 
   resources :games do
     resources :comments
   end
+  resources :users
 
   root 'home#index'
 end
